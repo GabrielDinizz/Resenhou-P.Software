1d7c72f88f3024b941217c08f779d9d210da70ff
GabrielDinizz <gabrieldinizcarreiro@gmail.com>
2024-05-27 22:07:56 -0300

    feat(routes): adicionando rota raiz

---
+++ app/Http/Controllers/TelaPrincipalController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TelaPrincipalController extends Controller
+{
+    public function mostrarView()
+    {
+        return view('telaPrincipal.tela');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TelaPrincipalController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [TelaPrincipalController::class, 'mostrarView']);
