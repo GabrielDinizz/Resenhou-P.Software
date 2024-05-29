cbccbb22f213cf99b2436583f6d0677173b57b6f
GabrielDinizz <gabrieldinizcarreiro@gmail.com>
2024-05-28 23:40:51 -0300

    feat: integração SAC footer com SqlLite

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TelaPrincipalController;
+use App\Http\Controllers\SacController;
 
+Route::get('/sac', [SacController::class, 'create'])->name('sac.create');
+Route::post('/sac', [SacController::class, 'store'])->name('sac.store');
 Route::get('/', [TelaPrincipalController::class, 'index']);
 
